818da15350041e0e3bae6bb5b75522b29dd35900
SunnySiddique <websunny56@gamil.com>
2024-10-01 21:15:03 +0600

    updated the component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import "swiper/css";
 import DetailPage from "./components/Detail/DetailPage";
 import Footer from "./components/Footer/Footer";
 import MainPage from "./components/MainPage";
